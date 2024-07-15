e063e99451c1402afcdff768cfa664321477026b
calm1205 <naginani072@gmail.com>
2024-07-15 10:45:05 +0900

    fix: update extention zip.js -> zip.cjs

--- electron-packager/zip.cjs
+++ electron-packager/zip.cjs
@@ -1,10 +1,10 @@
-import { zip } from "electron-installer-zip";
+const zip = require("electron-installer-zip");
 
 zip(
   {
     dir: "./release/min-electron-darwin-arm64/min-electron.app",
     // out can either be a directory or a path for a ZIP file
-    out: "./dist/",
+    out: "./app",
     // out: 'dist/App.zip',
   },
   (err, res) => {
